d0d5955f10567a0b69ad621a8b33b94ea486b1b7
Karl Kowal <kowal.karl99@gmail.com>
2024-11-19 13:25:49 +0100

    ändrade till fragment

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import { Navbar } from "./components/nav";
 import {Footer} from "./components/footer"
 function App() {
   return (
-    <div>
+    <>
       <Navbar/>
       <Footer/>
-    </div>
+    </>
   );
 }
 
