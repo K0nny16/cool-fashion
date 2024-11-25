f583199218e7eaf1b428d6b3f3630a2695f48bfc
K0nny16 <kowal.karl99@gmail.com>
2024-11-25 14:00:50 +0100

    fixade json

--- src/items.js
+++ src/items.js
@@ -3,50 +3,50 @@ const items = [
         "Herr": {
             "Skor": [
                 {
-                    id: 1,
-                    name: "testsko",
-                    pris: 70,
-                    date: "2023-03-03"
+                    "id": 1,
+                    "name": "testsko",
+                    "pris": 70,
+                    "date": "2023-03-03"
                 },
                 {
-                    id: 2,
-                    name: "snyggsko",
-                    pris: 150,
-                    date: "2023-04-01"
+                    "id": 2,
+                    "name": "snyggsko",
+                    "pris": 150,
+                    "date": "2023-04-01"
                 },
                 {
-                    id: 3,
-                    name: "löparsko",
-                    pris: 300,
-                    date: "2023-05-15"
+                    "id": 3,
+                    "name": "löparsko",
+                    "pris": 300,
+                    "date": "2023-05-15"
                 }
             ],
             "Kläder": [
                 {
-                    id: 1,
-                    name: "test",
-                    pris: 500,
-                    date: "2021-03-30"
+                    "id": 1,
+                    "name": "test",
+                    "pris": 500,
+                    "date": "2021-03-30"
                 }
             ]
-            }
         },
         "Dam": {
             "Skor": [
                 {
-                    id: 1,
-                    name: "test",
-                    pris: 799,
-                    date: "2020-01-02"
+                    "id": 1,
+                    "name": "test",
+                    "pris": 799,
+                    "date": "2020-01-02"
                 },
                 {
-                    id: 2,
-                    name: "sommarsko",
-                    pris: 899,
-                    date: "2022-06-15"
+                    "id": 2,
+                    "name": "sommarsko",
+                    "pris": 899,
+                    "date": "2022-06-15"
                 }
             ]
         }
     }
 ];
+
 
