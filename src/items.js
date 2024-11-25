46bf7926c50079c7ccc8a32266664bcd9175e093
K0nny16 <kowal.karl99@gmail.com>
2024-11-25 13:59:04 +0100

    fixade json

--- src/items.js
+++ src/items.js
@@ -1,26 +1,52 @@
 const items = [
     {
         "Herr": {
-            "Skor": {
-                id: 1,
-                name: "testsko",
-                pris: 70,
-                date: "2023-03-03"
-            },
-            "Kläder": {
-                id: 1,
-                name: "test",
-                pris: 500,
-                date: "2021-03-30"
+            "Skor": [
+                {
+                    id: 1,
+                    name: "testsko",
+                    pris: 70,
+                    date: "2023-03-03"
+                },
+                {
+                    id: 2,
+                    name: "snyggsko",
+                    pris: 150,
+                    date: "2023-04-01"
+                },
+                {
+                    id: 3,
+                    name: "löparsko",
+                    pris: 300,
+                    date: "2023-05-15"
+                }
+            ],
+            "Kläder": [
+                {
+                    id: 1,
+                    name: "test",
+                    pris: 500,
+                    date: "2021-03-30"
+                }
+            ]
             }
         },
-        "Dam":{
-            "Skor":{
-                id:1,
-                name: "test",
-                pris: 799,
-                date: "2020-01-02"
-            }
+        "Dam": {
+            "Skor": [
+                {
+                    id: 1,
+                    name: "test",
+                    pris: 799,
+                    date: "2020-01-02"
+                },
+                {
+                    id: 2,
+                    name: "sommarsko",
+                    pris: 899,
+                    date: "2022-06-15"
+                }
+            ]
         }
     }
 ];
+
