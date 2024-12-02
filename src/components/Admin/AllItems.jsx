35f756a77e6eb8c91b878c70cddd0f6fbdfa5d12
AdamBarnell <151514125+AdamBarnell@users.noreply.github.com>
2024-12-02 08:53:04 +0100

    fixed contact-form

--- src/components/Admin/AllItems.jsx
+++ src/components/Admin/AllItems.jsx
@@ -1,54 +1,65 @@
 import { items } from "../../items.js";
-import React, {useState} from "react";
-import Popup from "reactjs-popup";
-import "reactjs-popup/dist/index.css"
+import React, { useState } from "react";
+
 export function AllItems() {
-    
-    const flattenItems = (items) => {
-        const result = [];
-        items.forEach((itemGroup)=>{
-            Object.entries(itemGroup).forEach(([category,subCategory])=>{
-                Object.entries(subCategory).forEach(([subCategory,products])=>{
-                    products.forEach((product)=>{
-                        result.push({
-                            id: product.id,
-                            img: product.img,
-                            title: product.name,
-                            price: product.pris,
-                            buttonText: "Köp",
-                        })
-                    })
-                })
-            })
-        })
-        return result;
-    }
+  const flattenItems = (items) => {
+    const result = [];
+    items.forEach((itemGroup) => {
+      Object.entries(itemGroup).forEach(([category, subCategory]) => {
+        Object.entries(subCategory).forEach(([subCategory, products]) => {
+          products.forEach((product) => {
+            result.push({
+              id: product.id,
+              img: product.img,
+              title: product.name,
+              price: product.pris,
+              buttonText: "Köp",
+            });
+          });
+        });
+      });
+    });
+    return result;
+  };
 
-    const [allItems, setAllItems] = useState(flattenItems(items));
+  const [allItems, setAllItems] = useState(flattenItems(items));
 
-    const cards = flattenItems(items);
-    if(cards.length === 0) return(<p>Inga produkter hittades!</p>)
-    console.log("Flattend items: ",cards)
-    
-    return (
-        <div className="content">
-          <h2 className="title-AllItems" style={{textAlign:"center",fontWeight:"bold",textTransform:"uppercase"}} >Alla Produkter</h2>
-          <button className="add-item">Lägg till en ny produkt</button>
-          <div className="content-cards">
-            {cards.map((card) => (
-              <div key={card.id} className="content-card">
-                <img src={card.img} alt={card.title} className="content-card-image" />
-                <h3 className="content-card-title">{card.title}</h3>
-                <p className="content-card-price">{card.price} SEK</p>
-                <button
-                  className="content-card-button"
-                  onClick={() => alert(`${card.title} Köpt!`)}
-                >
-                    Köp
-                </button>
-              </div>
-            ))}
+  const cards = flattenItems(items);
+  if (cards.length === 0) return <p>Inga produkter hittades!</p>;
+  console.log("Flattend items: ", cards);
+
+  return (
+    <div className="content">
+      <h2
+        className="title-AllItems"
+        style={{
+          textAlign: "center",
+          fontWeight: "bold",
+          textTransform: "uppercase",
+        }}
+      >
+        Alla Produkter
+      </h2>
+      <button className="add-item">Lägg till en ny produkt</button>
+      <div className="content-cards">
+        {cards.map((card) => (
+          <div key={card.id} className="content-card">
+            <img
+              src={card.img}
+              alt={card.title}
+              className="content-card-image"
+            />
+            <h3 className="content-card-title">{card.title}</h3>
+            <p className="content-card-price">{card.price} SEK</p>
+            <button
+              className="content-card-button"
+              onClick={() => alert(`${card.title} Köpt!`)}
+            >
+              Köp
+            </button>
           </div>
-        </div>
-      );
+        ))}
+      </div>
+    </div>
+  );
 }
