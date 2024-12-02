c86d5b01637a75d3c29d030e5862dbcca5ec56c8
K0nny16 <kowal.karl99@gmail.com>
2024-12-02 14:04:38 +0100

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsc_ZVxBuhLfKhjkeCtMLFnfk1tb6FOk0",
+  authDomain: "cool-fashion-85a8e.firebaseapp.com",
+  projectId: "cool-fashion-85a8e",
+  storageBucket: "cool-fashion-85a8e.firebasestorage.app",
+  messagingSenderId: "527073420331",
+  appId: "1:527073420331:web:f60f05f3cb42f5eb55366e"
+};
+
+const app = initializeApp(firebaseConfig);
