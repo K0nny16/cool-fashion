132f06d16208eb7f4595c3f7f095f3c66d193c43
Karl Kowal <kowal.karl99@gmail.com>
2024-12-05 18:10:57 +0100

    sida med total quant och 10 senaste produkterna

--- src/nav.js
+++ src/nav.js
@@ -49,7 +49,7 @@ export async function fetchMenuData(adminState) {
           "Se Alla Produkter",
           "Tickets",
           "Kolla Lager",
-          "Totalt lager",
+          "Totalt Lager",
         ],
       });
     }
